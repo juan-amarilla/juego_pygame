eda63e790496c56d7dcb8a63780dea28877154bd
juan amarilla <juanamarillasebastian@gmail.com>
2024-11-14 22:05:40 -0300

    agregado

--- iniciar_juego.py
+++ iniciar_juego.py
@@ -1,1 +1,24 @@
-print("aca inicia el juego")
+import pygame
+from funciones import *
+from constantes import *
+
+iniciar()
+
+pantalla = pygame.display.set_mode((800, 600))
+colocar_titulo("el juego")
+
+fuente = pygame.font.Font(None, 50)
+
+opcion = 0
+
+ejecutar = True
+
+while ejecutar:
+
+    ejecutar = menu(fuente, ejecutar)
+                    
+    pantalla.fill(COLOR_FONDO)
+
+    colocar_opciones(fuente, pantalla)
+
+    pygame.display.flip()
